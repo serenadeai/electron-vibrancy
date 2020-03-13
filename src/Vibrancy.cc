dc007f9dca064c0bc39abbd0f9cdc0ee6841bdd0
moon-hyunsick <psymhs@gmail.com>
2020-03-13 18:27:38 +0900

    fix using deprecated v8 BooleanValue API  - https://v8docs.nodesource.com/node-12.0/dc/d0a/classv8_1_1_value.html#a9b14a81dc04004a86965ea7e3106a21f test with node v13.9.0, electron v8.1.1

--- src/Vibrancy.cc
+++ src/Vibrancy.cc
@@ -25,125 +25,133 @@
 #include "./Vibrancy.h"
 
 //----------------------------------------------------------------------------
-namespace Vibrancy {
-    static VibrancyHelper vibHelper_;
+namespace Vibrancy
+{
+static VibrancyHelper vibHelper_;
 
-    Vibrancy::Vibrancy() {
-    }
+Vibrancy::Vibrancy()
+{
+}
 
-    Vibrancy::~Vibrancy() {
-    }
-    void Vibrancy::Init(Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE target) {
-        v8::Local<v8::FunctionTemplate> tpl =
-            Nan::New<v8::FunctionTemplate>(SetVibrancy);
+Vibrancy::~Vibrancy()
+{
+}
+void Vibrancy::Init(Nan::ADDON_REGISTER_FUNCTION_ARGS_TYPE target)
+{
+    v8::Local<v8::FunctionTemplate> tpl =
+        Nan::New<v8::FunctionTemplate>(SetVibrancy);
 
-        tpl->InstanceTemplate()->SetInternalFieldCount(1);
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-        v8::Local<v8::FunctionTemplate> tpl1 =
-            Nan::New<v8::FunctionTemplate>(AddView);
-        tpl1->InstanceTemplate()->SetInternalFieldCount(1);
+    v8::Local<v8::FunctionTemplate> tpl1 =
+        Nan::New<v8::FunctionTemplate>(AddView);
+    tpl1->InstanceTemplate()->SetInternalFieldCount(1);
 
-        v8::Local<v8::FunctionTemplate> tpl2 =
-            Nan::New<v8::FunctionTemplate>(UpdateView);
-        tpl2->InstanceTemplate()->SetInternalFieldCount(1);
+    v8::Local<v8::FunctionTemplate> tpl2 =
+        Nan::New<v8::FunctionTemplate>(UpdateView);
+    tpl2->InstanceTemplate()->SetInternalFieldCount(1);
 
-        v8::Local<v8::FunctionTemplate> tpl3 =
-            Nan::New<v8::FunctionTemplate>(RemoveView);
-        tpl3->InstanceTemplate()->SetInternalFieldCount(1);
+    v8::Local<v8::FunctionTemplate> tpl3 =
+        Nan::New<v8::FunctionTemplate>(RemoveView);
+    tpl3->InstanceTemplate()->SetInternalFieldCount(1);
 
-        Nan::Set(target,
-            Nan::New("SetVibrancy").ToLocalChecked(),
-            Nan::GetFunction(tpl).ToLocalChecked());
+    Nan::Set(target,
+             Nan::New("SetVibrancy").ToLocalChecked(),
+             Nan::GetFunction(tpl).ToLocalChecked());
 
-        Nan::Set(target,
-            Nan::New("AddView").ToLocalChecked(),
-            Nan::GetFunction(tpl1).ToLocalChecked());
+    Nan::Set(target,
+             Nan::New("AddView").ToLocalChecked(),
+             Nan::GetFunction(tpl1).ToLocalChecked());
 
-        Nan::Set(target,
-            Nan::New("UpdateView").ToLocalChecked(),
-            Nan::GetFunction(tpl2).ToLocalChecked());
+    Nan::Set(target,
+             Nan::New("UpdateView").ToLocalChecked(),
+             Nan::GetFunction(tpl2).ToLocalChecked());
 
-        Nan::Set(target,
-            Nan::New("RemoveView").ToLocalChecked(),
-            Nan::GetFunction(tpl3).ToLocalChecked());
-    }
+    Nan::Set(target,
+             Nan::New("RemoveView").ToLocalChecked(),
+             Nan::GetFunction(tpl3).ToLocalChecked());
+}
 
-    NAN_METHOD(Vibrancy::SetVibrancy) {
-        v8::Local<v8::Object> toggleStateObj =
-            info[0].As<v8::Object>();
-        v8::Local<v8::Object> handleBuffer =
-            info[1].As<v8::Object>();
+NAN_METHOD(Vibrancy::SetVibrancy)
+{
+    v8::Local<v8::Object> toggleStateObj =
+        info[0].As<v8::Object>();
+    v8::Local<v8::Object> handleBuffer =
+        info[1].As<v8::Object>();
 
-        v8::Isolate* isolate = info.GetIsolate();
-        v8::HandleScope scope(isolate);
+    v8::Isolate *isolate = info.GetIsolate();
+    v8::HandleScope scope(isolate);
 
-        if (toggleStateObj->IsNull())
-            return;
+    if (toggleStateObj->IsNull())
+        return;
 
-        if (handleBuffer->IsNull())
-            return;
+    if (handleBuffer->IsNull())
+        return;
 
-        bool toggleState = toggleStateObj->BooleanValue();
+    bool toggleState = toggleStateObj->BooleanValue(isolate);
 
-        char* bufferData = node::Buffer::Data(handleBuffer);
+    char *bufferData = node::Buffer::Data(handleBuffer);
 
-        bool result = false;
+    bool result = false;
 
-        if (!toggleState)
-            result = vibHelper_.DisableVibrancy((unsigned char*)bufferData);
+    if (!toggleState)
+        result = vibHelper_.DisableVibrancy((unsigned char *)bufferData);
 
-        info.GetReturnValue().Set(result);
-    }
+    info.GetReturnValue().Set(result);
+}
 
-    NAN_METHOD(Vibrancy::AddView) {
-        v8::Local<v8::Object> handleBuffer = info[0].As<v8::Object>();
-        v8::Local<v8::Array> options = info[1].As<v8::Array>();
+NAN_METHOD(Vibrancy::AddView)
+{
+    v8::Local<v8::Object> handleBuffer = info[0].As<v8::Object>();
+    v8::Local<v8::Array> options = info[1].As<v8::Array>();
 
-        v8::Isolate* isolate = info.GetIsolate();
-        v8::HandleScope scope(isolate);
+    v8::Isolate *isolate = info.GetIsolate();
+    v8::HandleScope scope(isolate);
 
-        char* bufferData = node::Buffer::Data(handleBuffer);
+    char *bufferData = node::Buffer::Data(handleBuffer);
 
-        int32_t result = -1;
+    int32_t result = -1;
 
-        result = vibHelper_.AddView((unsigned char*)bufferData, options);
+    result = vibHelper_.AddView((unsigned char *)bufferData, options);
 
-        info.GetReturnValue().Set(result);
-    }
+    info.GetReturnValue().Set(result);
+}
 
-    NAN_METHOD(Vibrancy::UpdateView) {
-        v8::Local<v8::Object> handleBuffer =
-            info[0].As<v8::Object>();
-        v8::Local<v8::Array> options =
-            info[1].As<v8::Array>();
+NAN_METHOD(Vibrancy::UpdateView)
+{
+    v8::Local<v8::Object> handleBuffer =
+        info[0].As<v8::Object>();
+    v8::Local<v8::Array> options =
+        info[1].As<v8::Array>();
 
-        v8::Isolate* isolate = info.GetIsolate();
-        v8::HandleScope scope(isolate);
+    v8::Isolate *isolate = info.GetIsolate();
+    v8::HandleScope scope(isolate);
 
-        char* bufferData = node::Buffer::Data(handleBuffer);
+    char *bufferData = node::Buffer::Data(handleBuffer);
 
-        bool result = false;
+    bool result = false;
 
-        result = vibHelper_.UpdateView((unsigned char*)bufferData, options);
+    result = vibHelper_.UpdateView((unsigned char *)bufferData, options);
 
-        info.GetReturnValue().Set(result);
-    }
+    info.GetReturnValue().Set(result);
+}
 
-    NAN_METHOD(Vibrancy::RemoveView) {
-        v8::Local<v8::Object> handleBuffer =
-            info[0].As<v8::Object>();
-        v8::Local<v8::Array> options =
-            info[1].As<v8::Array>();
+NAN_METHOD(Vibrancy::RemoveView)
+{
+    v8::Local<v8::Object> handleBuffer =
+        info[0].As<v8::Object>();
+    v8::Local<v8::Array> options =
+        info[1].As<v8::Array>();
 
-        v8::Isolate* isolate = info.GetIsolate();
-        v8::HandleScope scope(isolate);
+    v8::Isolate *isolate = info.GetIsolate();
+    v8::HandleScope scope(isolate);
 
-        char* bufferData = node::Buffer::Data(handleBuffer);
+    char *bufferData = node::Buffer::Data(handleBuffer);
 
-        bool result = false;
+    bool result = false;
 
-        result = vibHelper_.RemoveView((unsigned char*)bufferData, options);
+    result = vibHelper_.RemoveView((unsigned char *)bufferData, options);
 
-        info.GetReturnValue().Set(result);
-    }
-}   //  namespace Vibrancy
+    info.GetReturnValue().Set(result);
+}
+} //  namespace Vibrancy
